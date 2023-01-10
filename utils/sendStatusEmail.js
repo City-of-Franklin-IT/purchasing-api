748109f80c29f43a4be3c9b2cdb9d7b690a16a61
asouthern729 <andrew.southern@franklintn.gov>
2023-01-10 16:05:51 -0600

    Adjustment to sendStatusEmail

--- utils/sendStatusEmail.js
+++ utils/sendStatusEmail.js
@@ -1,7 +1,12 @@
 const sendEmail = require('./sendEmail')
 
-const sendStatusEmail = async (request, newStatus) => {
-  const message = `Request ${request.requestId} - "${request.requestDetails}" status has changed from "${request.status}" to "${newStatus}".`
+const sendStatusEmail = async (request, newStatus, url) => {
+  let message
+  if(newStatus === 'Approved') {
+    message = `Request ${request.requestId} - "${request.requestDetails}" status has changed from "${request.status}" to "${newStatus}".\n\nPlease visit ${url} to submit purchase.`
+  } else if(newStatus === 'Approved For AP') {
+    message = `Request ${request.requestId} - "${request.requestDetails}" status has changed from "${request.status}" to "${newStatus}".\n\nPurchase will be completed by Finance Department - no additional actions are currently necessary.`
+  } else message = `Request ${request.requestId} - "${request.requestDetails}" status has changed from "${request.status}" to "${newStatus}".`
 
   await sendEmail({
     email: request.createdBy,
